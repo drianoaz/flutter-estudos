23e9698461d0fb802dbd0eec84baaa590acd318e
Adriano de Azevedo <contato@adriano.dev>
2020-05-07 21:24:11 -0300

    feat: add arithmetic operators codelab

---
+++ codelabs/arithmetic_operators.dart
@@ -0,0 +1,17 @@
+// trabalhar com números é igual a toda e qualquer linguagem que pareça com C
+
+void main() {
+
+  double num1 = 10.0;
+  double num2 = 25;
+
+  double result = (num1 + num2) - 15;
+
+  // num1 = num1 + 15;
+
+  num1 += 15;
+  num1++;
+
+  print(result);
+
+}
