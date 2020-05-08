1e8c33d3fb60c1e46bcd9e6c0b65a0df67e16d92
Adriano de Azevedo <contato@adriano.dev>
2020-05-07 21:49:21 -0300

    feat: add loops codelab

---
+++ codelabs/loops.dart
@@ -0,0 +1,20 @@
+void main() {
+
+  for(int i = 20; i >= 10; i--) {
+    print(i);
+  }
+
+  int j = 0;
+  while(j < 10) {
+    print(j);
+    j++;
+  }
+
+  int k = 0;
+
+  do {
+    print(k);
+    k++;
+  } while (k <= 0);
+
+}
