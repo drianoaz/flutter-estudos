af98c337cb24ffce69f11ef57491a9449e278d31
Adriano de Azevedo <contato@adriano.dev>
2020-05-07 21:15:59 -0300

    feat: add hello world codelab

---
+++ codelabs/hello_world.dart
@@ -0,0 +1,7 @@
+// A anatomia de um código dart é parecido com C
+// a execução do "programa" começa apartir do metodo main
+// todos os comandos devem finalizar com um ";"
+
+void main() {
+  print("hello world");
+}
