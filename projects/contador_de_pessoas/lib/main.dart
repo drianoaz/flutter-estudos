e09c6358f7ca84af19fad0e6061b8f1552ab5771
Adriano de Azevedo <contato@adriano.dev>
2020-05-09 10:33:33 -0300

    chore: add static layout

--- projects/contador_de_pessoas/lib/main.dart
+++ projects/contador_de_pessoas/lib/main.dart
@@ -1,13 +1,73 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title: 'Contador de pessoas',
-      home: Container(
-        child: Text('Testando isso aqui'),
+      home: Stack(
+        children: <Widget>[
+          Image.asset(
+            "images/restaurant.jpg",
+            fit: BoxFit.cover,
+            height: 1000,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoas: 0",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      onPressed: ()  {
+
+                      },
+                      child: Text(
+                        "+1",
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        "-1",
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "Pode entrar",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ],
+          )
+        ],
       ),
-    )
+    ),
   );
 }
