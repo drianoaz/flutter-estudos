436a6c09d22bcb5180288c04173b84025e4621e6
Adriano de Azevedo <contato@adriano.dev>
2020-05-09 11:07:23 -0300

    test: home widget

--- projects/contador_de_pessoas/test/widget_test.dart
+++ projects/contador_de_pessoas/test/widget_test.dart
@@ -1,30 +1,45 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:contador_de_pessoas/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Home());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Pessoas: 0'), findsOneWidget);
+    expect(find.text('Pode entrar'), findsOneWidget);
+
+    // Tap the '-' icon and trigger a frame.
+    await tester.tap(find.bySemanticsLabel("-1"));
+    await tester.pump();
+
+    // Verify that our counter has decremented.
+    expect(find.text('Pessoas: -1'), findsOneWidget);
+    expect(find.text('Mundo invertido?!'), findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.tap(find.bySemanticsLabel("+1"));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Pessoas: 10'), findsOneWidget);
+    expect(find.text('Pode entrar'), findsOneWidget);
+
+    await tester.tap(find.bySemanticsLabel("+1"));
+    await tester.pump();
+
+    expect(find.text('Pessoas: 11'), findsOneWidget);
+    expect(find.text('Lotado!'), findsOneWidget);
   });
 }
