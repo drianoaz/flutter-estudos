b3f20a8aebb9dde2e1a83df80644ca56e3c56c66
Adriano de Azevedo <contato@adriano.dev>
2020-05-10 00:08:33 -0300

    chore: finish layout

--- projects/calculadora_de_imc/lib/main.dart
+++ projects/calculadora_de_imc/lib/main.dart
@@ -28,6 +28,78 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(
+              Icons.person_outline,
+              size: 120,
+              color: Colors.green,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Peso (kg)",
+                labelStyle: TextStyle(color: Colors.green),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green),
+                ),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.green,
+              ),
+              cursorColor: Colors.green,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.green),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green),
+                ),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.green,
+              ),
+              cursorColor: Colors.green,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 10.0,
+              ),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  color: Colors.green,
+                  onPressed: () {},
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              "Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 25.0,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
