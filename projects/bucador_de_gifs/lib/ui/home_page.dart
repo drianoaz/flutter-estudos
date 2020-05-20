b118786104cde2bf2aa2b8f2d05db8ea7af04eae
Adriano de Azevedo <contato@adriano.dev>
2020-05-19 21:25:39 -0300

    fix: offset bug

--- projects/bucador_de_gifs/lib/ui/home_page.dart
+++ projects/bucador_de_gifs/lib/ui/home_page.dart
@@ -51,6 +51,7 @@ class _HomePageState extends State<HomePage> {
               onSubmitted: (text) {
                 setState(() {
                   _search = text;
+                  _offset = 0;
                 });
               },
             ),
