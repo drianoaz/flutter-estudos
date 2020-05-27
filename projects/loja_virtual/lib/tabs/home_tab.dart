51bd0e1cbb83727acb8cba3d58f50c4485d37889
Adriano de Azevedo <contato@adriano.dev>
2020-05-27 09:11:37 -0300

    chore: add staggered grid

--- projects/loja_virtual/lib/tabs/home_tab.dart
+++ projects/loja_virtual/lib/tabs/home_tab.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   Widget _buildBodyBack() {
@@ -35,7 +37,10 @@ class HomeTab extends StatelessWidget {
               ),
             ),
             FutureBuilder<QuerySnapshot>(
-              future: Firestore.instance.collection('home').orderBy('pos').getDocuments(),
+              future: Firestore.instance
+                  .collection('home')
+                  .orderBy('pos')
+                  .getDocuments(),
               builder: (BuildContext context,
                   AsyncSnapshot<QuerySnapshot> snapshot) {
                 if (!snapshot.hasData) {
@@ -49,11 +54,20 @@ class HomeTab extends StatelessWidget {
                     ),
                   );
                 } else {
-                  print(snapshot.data.documents.length);
-                  return SliverToBoxAdapter(
-                    child: Container(
-                      color: Colors.red,
-                    ),
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.documents.map((doc) {
+                      return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                    }).toList(),
+                    children: snapshot.data.documents.map((doc) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: doc.data['image'],
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
                   );
                 }
               },
