28c710ab716989558257ee3fbdd5571959645d41
Adriano de Azevedo <contato@adriano.dev>
2020-06-08 08:33:53 -0300

    chore: add products tab

--- projects/loja_virtual/lib/screens/home_screen.dart
+++ projects/loja_virtual/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,6 +15,14 @@ class HomeScreen extends StatelessWidget {
       children: <Widget>[
         Scaffold(
           body: HomeTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          body: ProductsTab(),
           drawer: CustomDrawer(_pageController),
         ),
       ],
---
+++ projects/loja_virtual/lib/tabs/products_tab.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class ProductsTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<QuerySnapshot>(
+      future: Firestore.instance.collection("products").getDocuments(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          return Container(
+            color: Colors.red,
+          );
+        }
+      },
+    );
+  }
+}
